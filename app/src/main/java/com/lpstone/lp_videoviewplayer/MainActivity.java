934d446b7842d8102b750654b05778e3f9eca220
LPStone <1216162643@qq.com>
2018-01-10 12:58:48 +0800

    Android Studio创建的空项目

---
+++ app/src/main/java/com/lpstone/lp_videoviewplayer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lpstone.lp_videoviewplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
